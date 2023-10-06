173b7783850c26708327f71f60cf0eda55bdd0c8
Jhon Guzman Simon <83370645+JhonGuzmanS@users.noreply.github.com>
2023-10-06 04:37:27 -0400

    Added app.py
    Holds the pages together, the main script.

---
+++ app.py
@@ -0,0 +1,19 @@
+import dash
+import pandas as pd
+from dash import Dash, html, dcc
+
+app = Dash(__name__, use_pages=True)
+
+app.layout = html.Div([
+    html.H1('Gambit Masters - Chess Dataset'),
+    html.Div([
+        html.Div(
+            dcc.Link(f"{page['name']}", href=page["relative_path"])
+        ) for page in dash.page_registry.values()
+    ]),
+    dash.page_container
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
